1810c85243a7c6e814da6a0c8523419218468280
Simon Crase <simon@greenweaves.nz>
2015-04-26 11:41:21 +1200

    Read features and build test and train data tables

--- run_analysis.R
+++ run_analysis.R
@@ -1,41 +1,53 @@
 rm(list=ls())
-data_directory<-"project"
 
-if(!file.exists(data_directory))
-  dir.create(data_directory)
+library(data.table)
 
-#0. Download file
-url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-base_file_name <- "UCI HAR Dataset"
-zip_file_name<-paste(base_file_name,".zip",sep="")
-full_zip_file_name<-file.path(data_directory,zip_file_name)
+read.features<-function(data_directory="data",base_file_name="UCI HAR Dataset") {
+  features<-read.table(file.path(data_directory,base_file_name="UCI HAR Dataset","features.txt"))
+  setnames(features,names(features),c("pos","Feature Name"))
+  return (as.vector(features[,2]))
+}
 
-download.file(url,destfile=full_zip_file_name,mode="wb")
-
-unzip(full_zip_file_name,exdir=data_directory)
+read.dataset<-function(mode,features,data_directory="data",base_file_name="UCI HAR Dataset"){
+  # Read the test dataset or training datset
+  #
+  # Args:
+  #   mode: Specified which dataset to load, either "test" or "train"
+  #   data_directory: The directory from which data will be loaded
+  #   base_file_name: The directory into which the zipfile has been expanded
+  #
+  # Returns:
+  #   A list containg X, y, and subject
+  
+  # Error handling
+  if(!file.exists(data_directory)) 
+    stop(paste("Data directory '",
+               data_directory,
+               "' does not exist. Run download_data.R.\n",
+               "NB Make sure both scripts are pointing at the same directory",
+               sep=""))
+  
+  X=read.table(file.path(data_directory,base_file_name,mode,paste("X_",mode,".txt",sep="")))
+  setnames(X,names(X),features)
+  y=read.table(file.path(data_directory,base_file_name,mode,paste("y_",mode,".txt",sep="")))
+  setnames(y,names(y),"activity")
+  subject=read.table(file.path(data_directory,base_file_name,mode,paste("subject_",mode,".txt",sep="")))
+  setnames(subject,names(subject),"subject")
+  return (cbind(subject,y,X))
+}
 
 #1. Merges the training and the test sets to create one data set.
+
+
+features=read.features()
+
+test<-read.dataset("test",features)
+
+train<-read.dataset("train",features)
+
+
 #2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 #3. Uses descriptive activity names to name the activities in the data set
 #4. Appropriately labels the data set with descriptive variable names. 
 #5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable
 #   for each activity and each subject.
-
-
-read.stuff<-function(mode="test"){
-  X=read.table(file.path(data_directory,base_file_name,mode,paste("X_",mode,".txt",sep="")))
-  y=read.table(file.path(data_directory,base_file_name,mode,paste("y_",mode,".txt",sep="")))
-  subject=read.table(file.path(data_directory,base_file_name,mode,paste("subject_",mode,".txt",sep="")))
-  return (list("X"=X,"y"=y,"subject"=subject))
-}
-
-test<-read.stuff()
-X_test<-test$X
-y_test<-test$y
-subject_test<-test$subject
-
-train<-read.stuff("train")
-X_train<-train$X
-y_train<-train$y
-subject_train<-train$subject
-
